fd08d558b21ec4d47289a709e236f77ebd25c8ce
binna <every5116@naver.com>
2025-07-06 05:37:35 +0900

    저작권 정리를 위한 리소스 제외, 중간 저장

---
+++ Assets/Scripts/Adventure/Monster/MonsterCore.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Knight.Adventure
+{
+    public abstract class MonsterCore : MonoBehaviour
+    {
+        public enum State
+        {
+            IDLE,
+            PATROL,
+            TRACE,
+            ATTACK
+        }
+        
+        [SerializeField] 
+        protected Image hpBar;
+        
+        [SerializeField]
+        private State state = State.IDLE;
+
+        [SerializeField] 
+        protected float _hp = 10f;
+        
+        [SerializeField] 
+        protected float _speed = 3f;
+        
+        [SerializeField]
+        public float _attackTime = 2f;
+
+        [SerializeField] 
+        protected float atkDamage = 5f;
+        
+        private const float HP = 10f;
+
+        protected Animator _animator;
+        protected Rigidbody2D _rigidbody;
+        protected Transform _playerTransform;
+        protected ItemManager _itemManager;
+        
+        protected float _toMonsterDistance;
+        protected bool _isTrace;
+        protected bool _isDead;
+        
+        protected abstract void Idle();
+        protected abstract void Patrol();
+        protected abstract void Trace();
+        protected abstract void Attack();
+
+        public void TakeDamage(float damage)
+        {
+            _hp -= damage;
+            hpBar.fillAmount = _hp / HP;
+
+            if (_hp <= 0)
+                Death();
+        }
+
+        void Death()
+        {
+            _isDead = true;
+            _animator.SetTrigger("Death");
+            gameObject.SetActive(false);
+            
+            // TODO 죽었을 때, 보여질 화면 작업 필요
+
+            int itemCount = Random.Range(0, 3);
+            for(int i = 0; i < itemCount; i++)
+                _itemManager.DripItem(transform.position);
+        }
+
+        protected void ChangeState(State newState)
+        {
+            state = newState;
+        }
+        
+        protected void Init(float hp, float speed, float attackTime, float damage)
+        {
+            _hp = hp;
+            _speed = speed;
+            _attackTime = attackTime;
+            _attackTime = damage;
+
+            _itemManager = FindFirstObjectByType<ItemManager>();
+            _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+            
+            _animator = GetComponent<Animator>();
+            _rigidbody = GetComponent<Rigidbody2D>();
+
+            // TODO ================================
+            _hp = HP;
+            hpBar.fillAmount = _hp / HP;
+
+            _isTrace = false;
+            _isDead = false;
+            /////////////////////////////////////////
+        }
+       
+        private void Update()
+        {
+            // 두 위치(플레이어 -> 몬스터) 간의 벡터(방향 + 거리 포함)
+            var toMonster = transform.position - _playerTransform.position;
+            
+            // 거리 추출
+            // 항상 양수
+            _toMonsterDistance = toMonster.magnitude;
+
+            // 정규화된 방향 벡터(길이는 1, 방향 정보만 유지)
+            var toMonsterDirection = toMonster.normalized;
+            
+            // 몬스터가 바라보는 방향 계산
+            // localScale.x가 양수면 오른쪽, 음수면 왼쪽을 바라보는 것으로 판단
+            // Vector3.right는 +X축 방향
+            var moveDirection = Vector3.right * transform.localScale.x;
+            
+            float dotValue = Vector3.Dot(moveDirection, toMonsterDirection);
+            
+            // 몬스터 기준, 플레이어가 시야각 안에 있는지 확인
+            _isTrace = dotValue < -0.5f;
+            
+            switch (state)
+            {
+                case State.IDLE:
+                    Idle();
+                    break;
+                case State.PATROL:
+                    // 정찰 기능
+                    Patrol();
+                    break;
+                case State.TRACE:
+                    // 추적 기능
+                    Trace();
+                    break;
+                case State.ATTACK:
+                    Attack();
+                    break;
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Ground") 
+                || other.gameObject.CompareTag("Player"))
+                return;
+            
+            var scaleX = transform.localScale.x * -1;
+            transform.localScale = new Vector3(scaleX, 1f, 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/KnightController_Keyboard.cs
@@ -0,0 +1,222 @@
+using Knight.Adventure;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Knight
+{
+    public class KnightController_Keyboard : MonoBehaviour
+    {
+        [SerializeField] 
+        private Image hpBar;
+        
+        [SerializeField] 
+        private float hp = HP;
+        
+        [SerializeField] 
+        private float moveSpeed = 3f;
+
+        [SerializeField] 
+        private float jumpPower = 13f;
+
+        [SerializeField] 
+        private float atkDamage = 3f;
+
+        private const float HP = 100f;
+        
+        private Vector2 _crouchOffset;
+        private Vector2 _crouchSize;
+        
+        private Vector2 _standOffset;
+        private Vector2 _standSize;
+        
+        private Animator _animator;
+        private Rigidbody2D _rigidbody;
+        private CapsuleCollider2D _collider;
+
+        private Vector3 _inputDir;
+        
+        private bool _isGround;
+        private bool _isAttack;
+        private bool _isCombo;
+        private bool _isLadder;
+
+        void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _collider = GetComponent<CapsuleCollider2D>();
+            
+            _crouchOffset = new Vector2(0.03912544f, 0.5389824f); 
+            _crouchSize = new Vector2(0.6767006f, 0.9599333f); 
+            
+            _standOffset  = new Vector2(0.03912544f, 0.7860222f);
+            _standSize  = new Vector2(0.6767006f, 1.400105f);
+        }
+
+        private void Update()       // 일반적인 작업
+        {
+            InputKeyboard();
+            Jump();
+            Attack();
+        }
+
+        private void FixedUpdate()  // 물리적인 작업
+        {
+            Move();
+        }
+
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Ground"))
+            {
+                _animator.SetBool("isGround", true);
+                _isGround = true;
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Ground"))
+            {
+                _animator.SetBool("isGround", false);
+                _isGround = false;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Monster"))
+            {
+                // TODO 몬스터 데미지
+                other.gameObject.GetComponent<MonsterCore>().TakeDamage(atkDamage);
+                other.gameObject.GetComponent<Animator>().SetTrigger("Hit");
+                
+                // TODO 방향 맞추고 튕기기(몬스터는 무조건 튕기기)
+                var scaleX = transform.localScale.x * -1;
+                other.gameObject.transform.localScale = new Vector3(scaleX, 1, 1);
+                return;
+            }
+
+            if (other.CompareTag("Ladder"))
+            {
+                _isLadder = true;
+                _rigidbody.gravityScale = 0f;
+                _rigidbody.linearVelocity = Vector2.zero;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Ladder"))
+            {
+                _isLadder = false;
+                _rigidbody.gravityScale = 2f;
+                _rigidbody.linearVelocity = Vector2.zero;
+            }
+        }
+
+        void InputKeyboard()
+        {
+            var horizontal = Input.GetAxisRaw("Horizontal");     // 좌, 우
+            var vertical = Input.GetAxisRaw("Vertical");         // 상, 하
+
+            _inputDir = new Vector3(horizontal, vertical, 0);
+            
+            _animator.SetFloat("PositionX", _inputDir.x);
+            _animator.SetFloat("PositionY", _inputDir.y);
+
+            if (vertical < 0)
+            {
+                _collider.size = _crouchSize;
+                _collider.offset = _crouchOffset;
+            }
+            else
+            {
+                _collider.size = _standSize;
+                _collider.offset = _standOffset;
+            }
+        }
+
+        void Move()
+        {
+            if (_isLadder)
+            {
+                _rigidbody.position = new Vector2(13.5f, _rigidbody.position.y);
+                _rigidbody.linearVelocityY = _inputDir.y * moveSpeed;
+            }
+            
+            if (_inputDir.x != 0)
+            {
+                _isLadder = false;
+                var scaleX = _inputDir.x > 0 ? 1f : -1f;
+                transform.localScale = new Vector3(scaleX, 1, 1);
+                
+                _rigidbody.linearVelocityX = _inputDir.x * moveSpeed;
+            }
+        }
+
+        void Jump()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && _isGround)
+            {
+                _animator.SetTrigger("Jump");
+                _rigidbody.AddForceY(jumpPower, ForceMode2D.Impulse);
+            }
+        }
+
+        void Attack()
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+
+                if (!_isAttack)
+                {
+                    _isAttack = true;
+                    atkDamage = 3f;
+                    _animator.SetTrigger("Attack");
+                    return;
+                }
+                
+                _isCombo = true;
+            }
+        }
+
+        // 공격 애니메이션에 이벤트로 연결
+        public void WaitCombo()
+        {
+            if (_isCombo)
+            {
+                atkDamage = 5f;
+                _animator.SetBool("isCombo", true);
+                return;
+            }
+        
+            _isAttack = false;
+            _animator.SetBool("isCombo", false);
+        }
+        
+        // 콤보 애니메이션에 이벤트로 연결
+        public void EndCombo()
+        {
+            _isAttack = false;
+            _isCombo = false;
+            _animator.SetBool("isCombo", false);
+        }
+
+        public void TakeDamage(float damage)
+        {
+            hp -= damage;
+            hpBar.fillAmount = hp / HP;
+            
+            if (hp <= 0f)
+                Death();
+        }
+
+        void Death()
+        {
+            _animator.SetTrigger("Death");
+            
+            // TODO 로비로 가는 로직을 만들어야 함
+        }
+    }
+}
